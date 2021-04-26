84faf995329b39122f740d9102f9d88810499d12
yongsik <mymyyoo@naver.com>
2021-04-27 08:31:05 +0900

    Modify github custom deploy process

--- .github/actions/build-dist-site/entrypoint.sh
+++ .github/actions/build-dist-site/entrypoint.sh
@@ -23,12 +23,13 @@ bundle install
 
 # Build the website using Jekyll
 echo "🏋️ Building website..."
-JEKYLL_ENV=production bundle exec jekyll build
+JEKYLL_ENV=production
+bundle exec jekyll build
 echo "Jekyll build done"
 
 # Now lets go to the generated folder by Jekyll
 # and perform everything else from there
-cd _posts
+cd _site
 
 echo "☁️ Publishing website"
 
